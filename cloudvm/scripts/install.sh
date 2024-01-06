7c7b7b0de7b36fc284004be050c6a945b048e564
Stancu Florin <niflostancu@gmail.com>
2024-01-06 16:30:00 +0200

    cloudvm: better cleanup & sysprep for cloud-init

--- cloudvm/scripts/install.sh
+++ cloudvm/scripts/install.sh
@@ -13,13 +13,20 @@ while [ "$(systemctl is-system-running 2>/dev/null)" != "running" ] && \
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y cloud-init cloud-utils cloud-initramfs-growroot
-# copy the cloud-init config
+
+# delete previous cloud-init generated files
+rm -f /etc/cloud/cloud.cfg.d/50-curtin-networking.cfg \
+	/etc/cloud/cloud.cfg.d/curtin-preserve-sources.cfg \
+	/etc/cloud/cloud.cfg.d/99-installer.cfg \
+	/etc/cloud/cloud.cfg.d/subiquity-disable-cloudinit-networking.cfg \
+	/etc/ssh/sshd_config.d/50-cloud-init.conf
+rm -f /etc/cloud/ds-identify.cfg
+rm -f /etc/netplan/*.yaml
+
+# copy our custom cloud-init config
 rsync -ai --chown="root:root" "$SRC/etc/" "/etc/"
 
-rm -f /etc/cloud/cloud.cfg.d/99-installer.cfg
-
 # disable ssh password authentication
-rm -f /etc/ssh/sshd_config.d/50-cloud-init.conf
 sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config
 
 # update grub to replace kernel cmdline
@@ -27,9 +34,11 @@ GRUB_CMDLINE_VIRT="modprobe.blacklist=floppy console=ttyS0,115200n8 no_timer_che
 sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_VIRT\"/g" /etc/default/grub
 update-grub
 
-# Cleanup the system
+# Cleanup & sysprep
 apt-get -y autoremove
 apt-get -y clean
-rm -rf /home/student/install*
+rm -rf /home/student/install* /home/student/.bash_history /root/.bash_history
 cloud-init clean --logs --machine-id
-
+rm -rf /var/lib/cloud/ /tmp/*
+rm -rf /var/log/*/*
+rm -f /var/log/* 2>/dev/null || true
