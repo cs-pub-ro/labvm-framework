37253207ecf367978c51ae77685c445fb70d18d8
Stancu Florin <niflostancu@gmail.com>
2024-12-27 14:10:07 +0200

    new generic layer packer build scripts (+ readme)
    Allows reusing the same Packer script as flexible layer: offers the
    possibility of syncing multiple directories inside /opt/vm-scripts and
    preparation + two-staged build allowing for a reboot between (for
    specific kernel/bootloader configuration use-cases).

---
+++ layers/generic/build.mk
@@ -0,0 +1,26 @@
+## Generic VM layer build macros
+
+## Variables  you can override them inside your Makefile):
+
+# scripts relative to the current makefile
+VM_GENERIC_SCRIPTS_DIR ?= scripts
+# generic layer's packer source dir
+VM_GENERIC_PKR_SRC ?= $(FRAMEWORK_DIR)/layers/generic
+# source (base) target to use
+VM_GENERIC_SRC_FROM ?= base
+# optional authorized keys (first one found, if any)
+VM_AUTHORIZED_KEYS ?= $(abspath $(firstword $(wildcard dist/*authorized_keys*)))
+
+# use with $(eval $(call vm_new_layer_generic,vm-id))
+define vm_new_layer_generic=
+$(1)-name ?= $(1)
+$(1)-packer-src = $$(VM_GENERIC_PKR_SRC)
+$(1)-packer-args ?=
+$(1)-packer-args += -var "vm_scripts_dir=" -var 'vm_scripts_list=$$(call \
+	_packer_json_list,$$($(1)-copy-scripts))'
+$(1)-packer-args += -var "vm_authorized_keys=$$(VM_AUTHORIZED_KEYS)"
+$(1)-copy-scripts ?= $(VM_GENERIC_SCRIPTS_DIR)
+$(1)-src-from ?= $$(VM_GENERIC_SRC_FROM)
+
+endef
+
