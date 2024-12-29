f4696d9ecbc8b0be4718dab88e0322703e2575a7
Stancu Florin <niflostancu@gmail.com>
2024-12-29 16:11:47 +0200

    example: unify reboot + kernel-cmdline

--- example/examplevm/scripts/install-stage1.d/19-kernel-cmdline.sh
+++ example/examplevm/scripts/install-stage1.d/19-kernel-cmdline.sh
@@ -2,7 +2,12 @@
 [[ -n "$__INSIDE_VM_RUNNER" ]] || { echo "Only call within VM runner!" >&2; return 1; }
 ## Kernel cmdline customizations
 
-KERNEL_CMDLINE_APPEND=" quiet"
+# append to previous cmdline (if any)
+KERNEL_CMDLINE_APPEND=${KERNEL_CMDLINE_APPEND:-}
+KERNEL_CMDLINE_APPEND+=" quiet"
 
 vm_run_script "common-snippets.d/kernel-cmdline"
+
+# and reboot!
+vm_run_script "common-snippets.d/reboot"
 
--- example/examplevm/scripts/install-stage1.d/20-reboot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[[ -n "$__INSIDE_VM_RUNNER" ]] || { echo "Only call within VM runner!" >&2; return 1; }
-## Runs the reboot snippet
-
-vm_run_script "common-snippets.d/reboot"
-
